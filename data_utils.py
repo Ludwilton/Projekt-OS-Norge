3d6bfcab00d237651c263f2581b7bc745b44563c
Philip Langpohl <philip.langpohl@outlook.com>
2024-11-13 10:30:47 +0100

    feat: made the function for plotting top medals useable

--- data_utils.py
+++ data_utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import hashlib as hl
+import seaborn as sns
 
 def read_athlete_events(file_path = "athlete_events.csv") -> pd.DataFrame:
     df = pd.read_csv(file_path)
@@ -21,28 +22,14 @@ def group_medals(df: pd.DataFrame, group_by="NOC"):
     return medal_counts
 
 
-def plot_top_medals(df: pd.DataFrame, limit=10, group_by='NOC'):
-    # Filtrera bort rader utan medaljer
-    df_medals = df.dropna(subset=['Medal'])
+def plot_top_medals(df: pd.DataFrame, limit=10, group_by='NOC') -> None:
+    medal_counts = group_medals(df, group_by)
 
-    medal_counts = df_medals.groupby(["NOC", "Medal"]).size().unstack(fill_value=0)
-    print(medal_counts)
-    return
+    medal_counts = medal_counts.head(limit)
+    ax = sns.barplot(medal_counts, y="Total", x="NOC", hue="NOC")
+    for container in ax.containers:
+        ax.bar_label(container)
 
-    # df_medals = df_medals[df_medals["Sport"] == "Football"]
-
-    medal_count = df_medals[group_by].value_counts()
-
-    # Välj de 10 länderna med flest medaljer
-    top_10_countries = medal_count.sort_values(ascending=False).head(limit)
-
-    # Skapa grafen
-    plt.figure(figsize=(10, 6))
-    top_10_countries.plot(kind='bar', color='skyblue')
-    plt.title('Topp 10 länder med flest OS-medaljer')
-    plt.xlabel('Land (NOC)')
-    plt.ylabel('Totalt antal medaljer')
-    plt.xticks(rotation=45)
     plt.show()
 
 
@@ -62,9 +49,11 @@ def hash_column(df, column):
 if __name__ == "__main__":
     df = pd.read_csv("athlete_events.csv")
     
-    medals_by_country = group_medals(df, group_by="NOC")
-    print(medals_by_country)
+    # medals_by_country = group_medals(df, group_by="NOC")
+    # print(medals_by_country)
 
-    df_norway = df[df["NOC"] == "NOR"]
-    norway_medals_per_sport = group_medals(df_norway, group_by="Sport")
-    print(norway_medals_per_sport)
+    # df_norway = df[df["NOC"] == "NOR"]
+    # norway_medals_per_sport = group_medals(df_norway, group_by="Sport")
+    # print(norway_medals_per_sport)
+
+    plot_top_medals(df)
