a6408548e9f001331d6e7e0e5d5903d3a12110fb
Ludwilton <ludwig.carlegrund@gmail.com>
2024-11-19 14:34:49 +0100

    updates to graph module, added subplot for sports

--- graph_module.py
+++ graph_module.py
@@ -1,6 +1,11 @@
 import plotly_express as px
 from data_utils import group_medals, filter_medal_entries
 import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+
 
 def norway_age_histogram(df):
     norway_athletes = df[df["NOC"] == "NOR"]
@@ -73,15 +78,34 @@ def age_distribution_by_sports(df):
 
 
 # ----- TODO fix graph function name
-def medal_distribution_by_country(df, sport="Alpine Skiing"): 
+def medal_distribution_by_country(df, sport="Alpine Skiing", subplot=False): 
     df_all_unique_participants = df.drop_duplicates(subset=["ID"])
     df_age_distribution_sports = df_all_unique_participants[df_all_unique_participants["Sport"].isin(["Alpine Skiing", "Gymnastics", "Football", "Shooting"])]
 
     medals_by_country = df_age_distribution_sports.groupby(["NOC", "Sport"])["Medal"].count().reset_index()
     all_medals_df = medals_by_country[medals_by_country["Medal"] > 0]
     df_dist_ = all_medals_df[all_medals_df["Sport"]== sport].sort_values(by="Medal", ascending=False)
-
-    return px.bar(df_dist_, x="NOC", y="Medal", color="NOC", labels={"Medal": "Medals"}, title=f"Medal distribution in {sport}")
+    if subplot == False:
+        return px.bar(df_dist_, x="NOC", y="Medal", color="NOC", labels={"Medal": "Medals"}, title=f"Medal distribution in {sport}")
+    elif subplot == True:
+        num_colors = len(df_dist_["NOC"])
+        colors = px.colors.qualitative.Plotly * (num_colors // len(px.colors.qualitative.Plotly) + 1)
+        return go.Bar(
+        x=df_dist_["NOC"],
+        y=df_dist_["Medal"],
+        name=sport,
+        marker=dict(color=colors[:num_colors])
+    )
+        
+        
+def subplot_medal_distribution(df, sport1, sport2, sport3, sport4):
+    fig = make_subplots(rows=2, cols=2, subplot_titles=[sport1, sport2, sport3, sport4])
+    fig.add_trace(medal_distribution_by_country(df, sport=sport1, subplot=True), row=1, col=1)
+    fig.add_trace(medal_distribution_by_country(df, sport=sport2, subplot=True), row=1, col=2)
+    fig.add_trace(medal_distribution_by_country(df, sport=sport3, subplot=True), row=2, col=1)
+    fig.add_trace(medal_distribution_by_country(df, sport=sport4, subplot=True), row=2, col=2)
+    fig.update_layout(title=f"Medal Distribution by Country for {sport1}, {sport2}, {sport3}, {sport4}", showlegend=False)
+    return fig
 
 
 def norway_medals_per_year(df):
@@ -115,7 +139,6 @@ def norway_medals_per_year(df):
         yaxis_title="Number of medals",
         xaxis_title="Year",
         legend_title_text="Category",
-        plot_bgcolor="white"
     )
 
     return fig
